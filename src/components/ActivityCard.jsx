0d4202faea6b39189098b1b87e28dbe84b2d163a
KrittinI <krittin.ikk@gmail.com>
2024-07-03 21:56:30 +0700

    remove duplicate data

--- src/components/ActivityCard.jsx
+++ src/components/ActivityCard.jsx
@@ -21,18 +21,11 @@ ChartJS.register(
 export default function ActivityCard() {
   const data = {
     labels: ['Users', 'Events', 'Questions', 'Rooms'],
-    labels: ['Users', 'Events', 'Questions', 'Rooms'],
     datasets: [
       {
         label: 'Users',
         data: [450, 350, 400, 200],
-        label: 'Users',
-        data: [450, 350, 400, 200],
         backgroundColor: [
-          '#60A5FA', // Blue
-          '#FFDA45', // Yellow
-          '#FB7185', // Red
-          '#4ADE80', // Green
           '#60A5FA', // Blue
           '#FFDA45', // Yellow
           '#FB7185', // Red

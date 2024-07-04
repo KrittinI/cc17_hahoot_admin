0afe458f2573b0f76b2e58ed96d46a7a41f23915
fhonthip1905 <fhonthip.yawichai@gmail.com>
2024-07-04 11:50:30 +0700

    modify-menu-bar

--- src/features/admin/components/AdminFeed.jsx
+++ src/features/admin/components/AdminFeed.jsx
@@ -6,11 +6,11 @@ export default function AdminFeed() {
   return (
     <div className='grid grid-cols-3 gap-x-2 gap-y-4 pt-8'>
       <div className='bg-white rounded-xl col-span-2 flex flex-col gap-2 p-4'>
-        <h1 className='text-[#343C6A] w-full text-base'>Total</h1>
+        <h1 className='text-font-title'>Total</h1>
         <BarChart />
       </div>
       <div className='bg-white rounded-xl col-span-1 flex flex-col gap-2 p-4 justify-between'>
-        <h1 className='text-[#343C6A] w-full text-base'> Topic Lists</h1>
+        <h1 className='text-font-title'> Topic Lists</h1>
         <DoughnutChart />
       </div>
       <div className='col-span-3'>

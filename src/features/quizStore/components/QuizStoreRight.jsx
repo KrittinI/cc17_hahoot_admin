872079149d6910190c710f4049ee0f1e20676cdd
KrittinI <krittin.ikk@gmail.com>
2024-07-07 22:53:58 +0700

    change doughnut onClick

--- src/features/quizStore/components/QuizStoreRight.jsx
+++ src/features/quizStore/components/QuizStoreRight.jsx
@@ -1,14 +1,39 @@
+import { useState } from "react";
 import DoughnutChart from "../../../components/DoughnutChart";
 import AdminSideBar from "../../admin/components/AdminSideBar";
 
 export default function QuizStoreRight({ topics, questions }) {
-
-    const lables = topics.map((topic) => topic.topicName);
+    const answers = {}
+    answers.amount = questions.reduce((a, c) => {
+        switch (c.answer) {
+            case "A": a[0]++
+                return a;
+            case "B": a[1]++
+                return a;
+            case "C": a[2]++
+                return a;
+            case "D": a[3]++
+                return a;
+        }
+    }, [0, 0, 0, 0])
+    answers.lables = ["A", "B", "C", "D"]
+    const topic = {}
+    topic.lables = topics.map((topic) => topic.topicName);
     const arr = new Array(topics.length).fill(0)
-    const amount = questions?.reduce((a, c) => {
+    topic.amount = questions?.reduce((a, c) => {
         a[c.topicId - 1]++
         return a
     }, arr);
+    const [data, setData] = useState({ ...topic })
+
+    const handleClickData = (name) => {
+        switch (name) {
+            case "answer": setData({ ...answers })
+                break;
+            case "topic": setData({ ...topic })
+        }
+    }
+
     return (
         <div>
             <div className="flex flex-col gap-2">
@@ -16,9 +41,9 @@ export default function QuizStoreRight({ topics, questions }) {
                     <h1 className="text-[#343C6A] w-full text-font-title">
                         Questions Statistics
                     </h1>
-                    <DoughnutChart amount={amount} lables={lables} />
+                    <DoughnutChart amount={data.amount} lables={data.lables} />
                 </div>
-                <AdminSideBar />
+                <AdminSideBar onClick={handleClickData} />
             </div>
         </div>
     )

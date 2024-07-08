76a0bf583e49271baeb3bed1e198215274faa6c4
fhonthip1905 <fhonthip.yawichai@gmail.com>
2024-07-08 16:27:01 +0700

    update-ActionStatus-Modal

--- src/features/quizStore/components/QuizStoreLeft.jsx
+++ src/features/quizStore/components/QuizStoreLeft.jsx
@@ -1,57 +1,89 @@
 import { useState } from "react";
 import Table from "../../../components/Table";
 import RowTableQuestions from "./RowTableQuestions";
+import useAdmin from "../../../hooks/useAdmin";
 
 export default function QuizStoreLeft({ questions, topics }) {
-    const newModel = questions.map(question => {
-        const newEvent = {}
-        newEvent.id = question.id
-        newEvent.title = question.question
-        newEvent.creator = question.user.username
-        newEvent.topicId = question.topicId
-        newEvent.quizList = question._count.assignOfBridges
-        return newEvent
-    })
-    const [sortedData, setSortedData] = useState(newModel)
-    const [sortConfig, setSortConfig] = useState({
-        key: null,
-        direction: true,
+  const { setQuestions } = useAdmin();
+  const newModel = questions.map((question) => {
+    const newEvent = {};
+    newEvent.id = question.id;
+    newEvent.title = question.question;
+    newEvent.creator = question.user.username;
+    newEvent.topicId = question.topicId;
+    newEvent.quizList = question._count.assignOfBridges;
+    return newEvent;
+  });
+  const [sortedData, setSortedData] = useState(newModel);
+  const [sortConfig, setSortConfig] = useState({
+    key: null,
+    direction: true,
+  });
+  const quizTable = [
+    { title: "No.", colSpan: 1, name: "id" },
+    { title: "Title", colSpan: 2, name: "title" },
+    { title: "Create By", colSpan: 1, name: "creator" },
+    { title: "Topic", colSpan: 1, name: "topicId" },
+    { title: "Using", colSpan: 1, name: "used" },
+    { title: "Action", colSpan: 1, name: "action" },
+  ];
+  const handleSort = (key, direction) => {
+    setSortConfig({ key, direction: !direction });
+    const sortingData = newModel.sort((a, b) => {
+      if (direction) {
+        if (a[key] < b[key]) return 1;
+        if (a[key] > b[key]) return -1;
+        if (a[key] === b[key]) return 0;
+      } else {
+        if (a[key] > b[key]) return 1;
+        if (a[key] < b[key]) return -1;
+        if (a[key] === b[key]) return 0;
+      }
     });
-    const quizTable = [
-        { title: 'No.', colSpan: 1, name: "id" },
-        { title: 'Title', colSpan: 2, name: "title" },
-        { title: 'Create By', colSpan: 1, name: "creator" },
-        { title: 'Topic', colSpan: 1, name: "topicId" },
-        { title: 'Using', colSpan: 1, name: "used" },
-        { title: 'Action', colSpan: 1, name: "action" },
-    ];
-    const handleSort = (key, direction) => {
-        setSortConfig({ key, direction: !direction });
-        const sortingData = newModel.sort((a, b) => {
-            if (direction) {
-                if (a[key] < b[key]) return 1
-                if (a[key] > b[key]) return -1
-                if (a[key] === b[key]) return 0
-            } else {
-                if (a[key] > b[key]) return 1
-                if (a[key] < b[key]) return -1
-                if (a[key] === b[key]) return 0
-            }
-        });
-        console.log(sortingData);
-        setSortedData(sortingData)
-    };
-    return (
-        <div>
-            <Table title={`Questions`} header={quizTable} gridCols={'7'} handleSort={handleSort} sortConfig={sortConfig}>
-                {sortedData.map((question) =>
-                    <RowTableQuestions
-                        key={question.id}
-                        item={[question.id, question.title, question.creator, topics[question.topicId - 1].topicName, question.quizList, question.isActive]}
-                        gridRowTable={'7'}
-                    />
-                )}
-            </Table>
-        </div>
-    )
+    setSortedData(sortingData);
+  };
+  const handleClickChangeStatus = (id) => {
+    const data = [...questions];
+    const mockdata = [...sortedData];
+    const foundIndex = data.findIndex((el) => el.id === id);
+    const foundMockIndex = mockdata.findIndex((el) => el.id === id);
+    data.splice(foundIndex, 1, {
+      ...data[foundIndex],
+      isActive: !data[foundIndex].isActive,
+    });
+    mockdata.splice(foundMockIndex, 1, {
+      ...mockdata[foundMockIndex],
+      isActive: !mockdata[foundMockIndex].isActive,
+    });
+    setQuestions(data);
+    setSortedData(mockdata);
+  };
+
+  return (
+    <div>
+      <Table
+        title={`Questions`}
+        header={quizTable}
+        gridCols={"7"}
+        handleSort={handleSort}
+        sortConfig={sortConfig}
+      >
+        {sortedData.map((question) => (
+          <RowTableQuestions
+            key={question.id}
+            item={[
+              question.id,
+              question.title,
+              question.creator,
+              topics[question.topicId - 1].topicName,
+              question.quizList,
+              question.isActive,
+            ]}
+            gridRowTable={"7"}
+            onConfirm={handleClickChangeStatus}
+          />
+        ))}
+      </Table>
+    </div>
+  );
 }
--- src/features/usersProfile/components/RowTableUser.jsx
+++ src/features/usersProfile/components/RowTableUser.jsx
@@ -4,24 +4,19 @@ import BannedUser from "../../../components/BannedUser";
 import { LockIcon, UnlockIcon } from "../../../icons/Banned";
 
 const gridMap = {
-  6: 'grid-cols-6',
-  7: 'grid-cols-7',
-  10: 'grid-cols-10',
-  12: 'grid-cols-12',
-}
+  6: "grid-cols-6",
+  7: "grid-cols-7",
+  10: "grid-cols-10",
+  12: "grid-cols-12",
+};
 
-
-export default function RowTableUser({
-  item,
-  gridRowTable,
-  onConfirm
-}) {
+export default function RowTableUser({ item, gridRowTable, onConfirm }) {
   const [open, setOpen] = useState(false);
 
   const onAgree = () => {
-    onConfirm(item[0])
-    setOpen(false)
-  }
+    onConfirm(item[0]);
+    setOpen(false);
+  };
   return (
     <div className={`grid ${gridMap[gridRowTable]} text-center py-4 border-b`}>
       <div className="col-span-1 text-font-body">{item[0]}</div>
@@ -30,16 +25,35 @@ export default function RowTableUser({
       <div className="col-span-1 text-font-body">{item[3]}</div>
       <div className="col-span-1 text-font-body">{item[4]}</div>
       <div role="button" className="col-span-1 text-font-body ">
-        <div className="flex justify-center items-center text-center " onClick={() => setOpen(true)}>{!item[5] ? <LockIcon /> : <UnlockIcon />}</div>
+        <div
+          className="flex justify-center items-center text-center "
+          onClick={() => setOpen(true)}
+        >
+          {!item[5] ? <LockIcon /> : <UnlockIcon />}
+        </div>
       </div>
       <div className="grid gap-4">
         <Modal
           open={open}
           onClose={() => setOpen(false)}
           title={
-            item[5]
-              ? `Are you want to Ban this user`
-              : `Are you want to Unban this user`
+            item[5] ? (
+              <div className="flex flex-col justify-center items-center gap-8">
+                <div className="w-[80%] text-center">Are you want to Ban this user</div>
+                <div className="w-[80px] h-[80px] border-4 border-red rounded-full justify-center items-center flex">
+                  <LockIcon size={12} />
+                </div>
+              </div>
+            ) : (
+
+              <div className="flex flex-col justify-center items-center gap-8">
+                <div className="w-[80%] text-center">Are you want to Unban this user</div>
+                <div className="w-[80px] h-[80px] border-4 border-darkgreen rounded-full justify-center items-center flex">
+                  <UnlockIcon size={12} />
+                </div>
+              </div>
+            
+            )
           }
         >
           <BannedUser

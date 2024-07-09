5e5c22c1d880e755a56e254a281ce51155f41362
Tanarat23 <je.tanarat@gmail.com>
2024-07-09 12:33:58 +0700

    roomNotError

--- src/features/rooms/components/RowTableRooms.jsx
+++ src/features/rooms/components/RowTableRooms.jsx
@@ -1,70 +1,62 @@
-import { useState } from "react";
-import Modal from "../../../components/Modal";
-import BannedUser from "../../../components/BannedUser";
-import { LockIcon, UnlockIcon } from "../../../icons/Banned";
+import { useState } from 'react';
+import Modal from '../../../components/Modal';
+import BannedUser from '../../../components/BannedUser';
+// import { LockIcon, UnlockIcon } from '../../../icons/Banned';
 
 const gridMap = {
-    6: 'grid-cols-6',
-    7: 'grid-cols-7',
-    10: 'grid-cols-10',
-    12: 'grid-cols-12',
-}
-
+  6: 'grid-cols-6',
+  7: 'grid-cols-7',
+  10: 'grid-cols-10',
+  12: 'grid-cols-12',
+};
 
 export default function RowTableRooms({
-    item,
-    onBanUser,
-    onUnBanUser,
-    setSelectedUser,
-    gridRowTable,
+  item,
+  onBanUser,
+  onUnBanUser,
+  gridRowTable,
 }) {
-    const [open, setOpen] = useState(false);
-    const [tableRow, setTableRow] = useState([]);
+  const [open, setOpen] = useState(false);
 
-    const handleBanClick = () => {
-        setSelectedUser(item);
-        setOpen(true);
-    };
+  const handleConfirmBan = () => {
+    onBanUser(item);
+    setOpen(false);
+  };
 
-    const handleConfirmBan = () => {
-        onBanUser(item);
-        setOpen(false);
-    };
+  const handleConfirmUnBan = () => {
+    onUnBanUser(item);
+    setOpen(false);
+  };
 
-    const handleConfirmUnBan = () => {
-        onUnBanUser(item);
-        setOpen(false);
-    };
-
-    return (
-        <div className={`grid ${gridMap[gridRowTable]} text-center py-4 border-b`}>
-            <div className="col-span-1 text-font-body">{item[0]}</div>
-            <div className="col-span-1 text-font-body">{item[1]}</div>
-            <div className="col-span-2 text-font-body">{item[2]}</div>
-            <div className="col-span-1 text-font-body">{item[3]}</div>
-            <div className="col-span-1 text-font-body">{item[4]}</div>
-            <div role="button" className="col-span-1 text-font-body ">
-                <div className="flex justify-center items-center text-center ">{item[5] ? <LockIcon /> : <UnlockIcon />}</div>
-            </div>
-            <div className="grid gap-4">
-                <Modal
-                    open={open}
-                    onClose={() => setOpen(false)}
-                    title={
-                        item.detail5 === "Active"
-                            ? `Are you want to Banning this user`
-                            : `Are you want to Unbanning this user`
-                    }
-                >
-                    <BannedUser
-                        onConfirm={
-                            item.detail5 === "Active" ? handleConfirmBan : handleConfirmUnBan
-                        }
-                        onCancel={() => setOpen(false)}
-                        isActive={item.detail5 === "Active"}
-                    />
-                </Modal>
-            </div>
+  return (
+    <div className={`grid ${gridMap[gridRowTable]} text-center py-4 border-b`}>
+      <div className='col-span-4 text-font-body'>{item[0]}</div>
+      <div className='col-span-4 text-font-body'>{item[1]}</div>
+      <div className='col-span-4 text-font-body'>{item[2]}</div>
+      {/* <div role='button' className='col-span-1 text-font-body '>
+        <div className='flex justify-center items-center text-center '>
+          {item[5] ? <LockIcon /> : <UnlockIcon />}
         </div>
-    );
+      </div> */}
+      <div className='grid gap-4'>
+        <Modal
+          open={open}
+          onClose={() => setOpen(false)}
+          title={
+            item.detail5 === 'Active'
+              ? `Are you want to Banning this user`
+              : `Are you want to Unbanning this user`
+          }
+        >
+          <BannedUser
+            onConfirm={
+              item.detail5 === 'Active' ? handleConfirmBan : handleConfirmUnBan
+            }
+            onCancel={() => setOpen(false)}
+            isActive={item.detail5 === 'Active'}
+          />
+        </Modal>
+      </div>
+    </div>
+  );
 }
